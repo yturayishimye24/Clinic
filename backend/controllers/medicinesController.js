f5e3c47e0d20c41aaaa5248d01c855512d28adce
yturayishimye24 <yturayishimye@gmail.com>
2026-05-05 16:28:45 +0200

    Progressing...

--- backend/controllers/medicinesController.js
+++ backend/controllers/medicinesController.js
@@ -5,7 +5,7 @@ export const createMedicine = async (req,res) =>{
     try{
     const {medicineName,category,dosage,quantity,expiryDate,instructions,sideEffects} = req.body;
      
-    const medicine= await Medicine.create({
+    const CreatedMedicine= await Medicine.create({
         medicineName,
         category,
         dosage,
@@ -15,10 +15,10 @@ export const createMedicine = async (req,res) =>{
         quantity,
     })
 
-    if(!medicine){
+    if(!CreatedMedicine){
         res.status(500).json({success:false,msg:"Erroring creating a medicine! Please try again later"})
     }else{
-        res.status(201).json({success:true,msg:"Medicine created successfully!"})
+        res.status(201).json({success:true,msg:"Medicine created successfully!"},{CreatedMedicine})
     }
 }catch(error){
     console.log("Error creating medicine!");
@@ -30,7 +30,7 @@ export const getMedicine = async(req,res) =>{
     try{
     const medicine = await Medicine.find();
     if(medicine){
-        res.status(200).json({success:true,msg:"Success in getting medicine"})
+        res.status(200).json({success:true,msg:"Success in getting medicine"},{medicine})
     }else{
         res.status(404).json({success:false,msg:"Error fetching medicines, please try again later!"})
     }
@@ -38,7 +38,7 @@ export const getMedicine = async(req,res) =>{
     console.log("Error fetching medicines");
     res.status(501).json({success:false,msg:"Error fetching medicines!"})
 }
-
+}
 export const deleteMedicine = async(req,res)=>{
     try{
       const medicineExist = await Medicine.find();
@@ -57,4 +57,26 @@ export const deleteMedicine = async(req,res)=>{
         res.status(500).json({success:false,msg:"Error deleting medicine"})
     }
 }
+
+export const updateMedicine = async(req,res) =>{
+    try{
+        const updatedMediciine = await Medicine.findByIdAndUpdate(req.params.id,{
+            medicineName,
+            category,
+            dosage,
+            expiryDate,
+            quantity,
+            instructions,
+            sideEffects,
+        },{new: true})
+        if(!updatedMedicine){
+            res.status(500).json({message:"Error updating",success:false})
+        }else{
+            res.json({updatedMedicine});
+        }
+    }catch(error){
+        console.log("Error updating medicine")
+        res.status(500).json({success:false,msg:"Error deleting mecidines"})
+    }
 }
+
